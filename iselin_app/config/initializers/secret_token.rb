75dc00d19fbe288badec0eb320344a6041092633
nikolasjansen <nikolas.jansen@gmail.com>
2012-08-27 21:35:56 +0200

    Updated with ror

---
+++ iselin_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IselinApp::Application.config.secret_token = 'a093db4ee3d6026f25d7579a9a56692488b0e478552a3645ec921b74f01d3c13702db9b1a04d508ffca27a454471fbd0720973f3f7e169d2a3d9b433cf5a4792'
