d5fa41dab315e49aeaaa8a63a8b900f7292f68a4
nikolasjansen <nikolas.jansen@gmail.com>
2012-09-17 20:47:08 +0200

    nesten fikset slider paa forside

--- iselin_app/app/controllers/home_controller.rb
+++ iselin_app/app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
+  
   def about
+    
   end
+  
+  def index
+    @images = Dir['app/assets/images/frontpage_slideshow/*.*'].map {|f| f.sub('app/assets/images/','') }
+    
+  end
+  
+  def _menu
+    
+  end
+  
+  def portofolio
+    
+  end
+  
+  def getHeight(picture)
+    image = ImageMagick::Image.open(picture.path)
+    return image[:width].to_i
+  end
+  
 end
